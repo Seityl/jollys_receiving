65001985f6fbc0fdffc418bad9c0fbf2120248ed
Seityl <francisbeatsproductions@gmail.com>
2025-04-04 12:01:57 -0400

    minor: updated auto reorder settings

--- jollys_receiving/public/set_picklists.py
+++ jollys_receiving/public/set_picklists.py
@@ -1,218 +1,198 @@
-import frappe
-import erpnext
 import datetime
+
+import frappe
 from frappe.utils import nowdate
+
+import erpnext
 from erpnext.stock.reorder_item import reorder_item
 
-# Run script using 'bench execute jollys_receiving.public.set_picklists.schedule_set_picklists' in frappe-bench directory
+# Run script using 'bench execute jollys_receiving.public.set_picklists.schedule_generate_auto_reorder_material_requests' in frappe-bench directory
 
-def create_auto_reorder_log(schedule: str, locations: str, message: str):
-    log = frappe.new_doc('Auto Reorder Log')
-    log.update({
-        'date': nowdate(),
-        'schedule': schedule,
-        'locations': locations,
-        'message': message
-    })
-    log.insert()
+def get_reorder_settings():
+    return frappe.get_single('Auto Reorder Settings')
     
 @frappe.whitelist()
-def schedule_set_picklists():
-    try:
-        reorder_settings = {
-            'schedule': {
-                'sunday': frappe.db.get_single_value('Auto Reorder Settings', 'sunday'),
-                'monday': frappe.db.get_single_value('Auto Reorder Settings', 'monday'),
-                'tuesday': frappe.db.get_single_value('Auto Reorder Settings', 'tuesday'),
-                'wednesday': frappe.db.get_single_value('Auto Reorder Settings', 'wednesday'),
-                'thursday': frappe.db.get_single_value('Auto Reorder Settings', 'thursday'),
-                'friday': frappe.db.get_single_value('Auto Reorder Settings', 'friday'),
-                'saturday': frappe.db.get_single_value('Auto Reorder Settings', 'saturday')
-            },
-            'locations': {
-                'GG Stock - JP' : frappe.db.get_single_value('Auto Reorder Settings', 'gg'),
-                'JPMini Stock - JP' : frappe.db.get_single_value('Auto Reorder Settings', 'mini'),
-                'JPPM Stock - JP' : frappe.db.get_single_value('Auto Reorder Settings', 'jppm'),
-                'KG Stock - JP' : frappe.db.get_single_value('Auto Reorder Settings', 'kg'),
-                'Mega Retail - JP' : frappe.db.get_single_value('Auto Reorder Settings', 'mega'),
-                'Mega Stock - JP': frappe.db.get_single_value('Auto Reorder Settings', 'mega_stock'),
-                'WS Stock - JP' : frappe.db.get_single_value('Auto Reorder Settings', 'ws')
-            }
-        }
+def schedule_generate_auto_reorder_material_requests():
+    reorder_settings = get_reorder_settings()
+    frappe.enqueue(
+        generate_auto_reorder_material_requests,
+        queue = 'long',
+        timeout = 3600,
+        is_async = True,
+        now = False, 
+        job_name = 'Generate Auto Reorder Material Requests',
+        reorder_settings = reorder_settings
+    )
 
-        frappe.enqueue(
-            set_picklists,
-            queue='default',
-            timeout=86400,
-            is_async=True,
-            now=False, 
-            job_name='Generate & Set Auto Reorders',
-            reorder_settings=reorder_settings
-        )
-
-        create_auto_reorder_log(schedule='', locations='', message='enqueued the generation of material requests')
-        return {
-            'status': 'success',
-            'message' : 'Enqueued the generation of material requests'
-        }
-        
-    except Exception as e:
-        create_auto_reorder_log(schedule='', locations='', message=f'something went wrong when enqueuing the generation of material requests: {e}')
-        return {
-            'status': 'error',
-            'message' : f'something went wrong when enqueuing the generation of material requests: {e}'
-        }
+def schedule_create_pick_lists(split_mr_list):
+    frappe.enqueue(
+        create_pick_lists,
+        queue = 'long',
+        timeout = 3600,
+        is_async = True,
+        now = False, 
+        job_name = 'Set Auto Reorder Pick Lists',
+        split_mr_list = split_mr_list
+    )
     
-def set_picklists(reorder_settings: dict):
-    schedule = [day for day, value in reorder_settings['schedule'].items() if value == 1]
-    locations = [location for location, value in reorder_settings['locations'].items() if value == 1]
-    today = datetime.date.today().strftime('%A').lower()
-    out = {
-        'schedule': ', '.join(schedule),
-        'locations': ', '.join(locations),
-        'message': ''
-    }
-    
-    if (today not in schedule):
-        out['message'] = f'skipped generation of auto reorders on {today}, {nowdate()}'
-        create_auto_reorder_log(out['schedule'], out['locations'], out['message'])
+def generate_auto_reorder_material_requests(reorder_settings):
+    schedule = [row.day for row in reorder_settings.schedule if row.active == 1]
+    locations = [row.location for row in reorder_settings.locations if row.active == 1]
+    today = datetime.date.today().strftime('%A')
+    if today not in schedule:
         return
 
     mr_list = reorder_item()
-    out['message'] += f'generated material requests: {mr_list}<br>'
     split_mr_list = []
 
     for mr in mr_list:
         mr_doc = frappe.get_doc('Material Request', mr.name)
-        try:
-            try:
-                if  mr_doc.material_request_type != 'Material Transfer':
-                    if mr_doc.docstatus == 0:
-                        mr_doc.delete() 
 
-                    elif mr_doc.docstatus == 1:
-                        mr_doc.docstatus = 2
-                        mr_doc.save()
-                        mr_doc.delete()
+        if  mr_doc.material_request_type != 'Material Transfer':
+            frappe.db.savepoint('sp1')
 
-                    out['message'] += f'deleted material request {mr_doc.name} of type {mr_doc.material_request_type}<br>'
+            if mr_doc.docstatus == 0 or mr_doc.docstatus == 2:
+                try:
+                    mr_doc.delete() 
+                    frappe.db.commit()
+
+                except Exception as e:
+                    frappe.db.rollback()
+                    frappe.log_error(frappe.get_traceback(), "Error in generate_auto_reorder_material_requests()")
                     continue
-
-            except Exception as e: 
-                out['message'] += f'ERROR: Something went wrong when deleting Material Request {mr_doc.name} of type {mr_doc.material_request_type}<br>'
-                continue
-            
-            doc_locations = []
-        
-            for item in mr_doc.items:
-                if item.warehouse not in doc_locations:
-                    doc_locations.append(item.warehouse)
-        
-            for location in doc_locations:
-                if location not in locations:
-                    out['message'] += f'skipped re-order for {location}<br>'
-                    continue
-                
-                new_material_request = frappe.new_doc("Material Request")
-                new_material_request.update({
-                    "company": erpnext.get_default_company() or frappe.db.sql("""select name from tabCompany limit 1""")[0][0],
-                    "schedule_date": nowdate(),
-                    "transaction_date": nowdate(),
-                    "material_request_type": mr_doc.material_request_type,
-                    'set_warehouse': location
-                })
-                
-                for item in mr_doc.items:
-                    if item.warehouse == location:
-                        new_material_request.append("items", {
-                            "item_code": item.item_code,
-                            "qty": item.qty,
-                            "warehouse": location,
-                        })
-
-                new_material_request.insert()
-                out['message'] += f'inserted material request for {location}<br>'
-                new_material_request.submit()
-                new_material_request.add_comment('Info', f'Auto-Reorders - {nowdate()} - {location}')
-                out['message'] += f'submitted material request for {location}<br>'
-                split_mr_list.append(new_material_request.name)
-
-            out['message'] += f'split material request {mr.name}<br>'
-            
-            if mr_doc.docstatus == 0:
-                mr_doc.delete() 
 
             elif mr_doc.docstatus == 1:
+                try:
+                    mr_doc.docstatus = 2
+                    mr_doc.save()
+                    mr_doc.delete()
+                    frappe.db.commit()
+
+                except Exception as e:
+                    frappe.db.rollback()
+                    frappe.log_error(frappe.get_traceback(), "Error in generate_auto_reorder_material_requests()")
+                    continue
+
+        doc_locations = []
+    
+        for item in mr_doc.items:
+            if item.warehouse not in doc_locations:
+                doc_locations.append(item.warehouse)
+
+        ordered_doc_locations = [loc for loc in locations if loc in doc_locations]
+
+        for location in ordered_doc_locations:
+            new_material_request = frappe.new_doc("Material Request")
+            new_material_request.update({
+                "company": erpnext.get_default_company() or frappe.db.sql("""select name from tabCompany limit 1""")[0][0],
+                "schedule_date": nowdate(),
+                "transaction_date": nowdate(),
+                "material_request_type": mr_doc.material_request_type,
+                'set_warehouse': location
+            })
+            
+            for item in mr_doc.items:
+                if item.warehouse == location:
+                    new_material_request.append("items", {
+                        "item_code": item.item_code,
+                        "qty": item.qty,
+                        "warehouse": location,
+                    })
+
+            frappe.db.savepoint('sp2')
+
+            try:
+                new_material_request.insert()
+                new_material_request.submit()
+                new_material_request.add_comment('Info', f'Auto-Reorders - {nowdate()} - {location}')
+                frappe.db.commit()
+
+            except Exception as e:
+                frappe.db.rollback()
+                frappe.log_error(frappe.get_traceback(), "Error in generate_auto_reorder_material_requests()")
+                continue
+
+            split_mr_list.append(new_material_request.name)
+
+        frappe.db.savepoint('sp3')
+        
+        if mr_doc.docstatus == 0 or mr_doc.docstatus == 2:
+            try:
+                mr_doc.delete()
+                frappe.db.commit()
+
+            except Exception as e:
+                frappe.db.rollback()
+                frappe.log_error(frappe.get_traceback(), "Error in generate_auto_reorder_material_requests()")
+
+        elif mr_doc.docstatus == 1:
+            try:
                 mr_doc.docstatus = 2
                 mr_doc.save()
                 mr_doc.delete()
+                frappe.db.commit()
 
-            out['message'] += f'deleted unsorted material request {mr.name}<br>'
-            ct = len(split_mr_list)
-            cur_ct = 1
+            except Exception as e:
+                frappe.db.rollback()
+                frappe.log_error(frappe.get_traceback(), "Error in generate_auto_reorder_material_requests()")
 
-            for mr in split_mr_list:
-                create_pick_lists(mr, out, ct, cur_ct)
-                cur_ct += 1
+    schedule_create_pick_lists(split_mr_list)
 
-            create_auto_reorder_log(out['schedule'], out['locations'], out['message'])
+def create_pick_lists(split_mr_list):
+    for mr in split_mr_list:
+        material_request = frappe.get_doc('Material Request', mr)
+        item_groups = []
 
-        except Exception as e:
-            out['message'] += f'ERROR: something went wrong when splitting material request{e}<br>'
-            create_auto_reorder_log(out['schedule'], out['locations'], out['message'])
-    
-@frappe.whitelist()
-def create_pick_lists(mr, out, ct, cur_ct):
-    material_request = frappe.get_doc('Material Request', mr)
-    item_groups = []
+        for item in material_request.items:
+            if item.item_group not in item_groups:
+                item_groups.append(item.item_group)
+                
+        for item_group in item_groups:
+            try:
+                sorted_pick_list = frappe.new_doc('Pick List')
+                sorted_pick_list.update({
+                    'purpose': material_request.material_request_type,
+                    'material_request': material_request.name,
+                    'parent_warehouse': 'KG Warehouse - JP',
+                    'custom_target_warehouse': material_request.set_warehouse,
+                    'custom_item_group': item_group
+                })
 
-    for item in material_request.items:
-        if item.item_group not in item_groups:
-            item_groups.append(item.item_group)
-            
-    pct = len(item_groups)
-    cur_pct = 1
+                for item in material_request.items:
+                    if item.item_group == item_group:
+                        sorted_pick_list.append('locations', {   
+                            'item_code': item.item_code,
+                            'stock_qty': item.qty,
+                            'material_request': material_request.name,
+                            'material_request_item': item.name,
+                            "conversion_factor": item.conversion_factor,
+                            "uom": item.uom
+                        })
 
-    for item_group in item_groups:
-        try:
-            sorted_pick_list = frappe.new_doc('Pick List')
-            sorted_pick_list.update({
-                'purpose': material_request.material_request_type,
-                'material_request': material_request.name,
-                'parent_warehouse': 'KG Warehouse - JP',
-                'custom_target_warehouse': material_request.set_warehouse,
-                'custom_item_group': item_group
-            })
+                frappe.db.savepoint('sp4')
 
-            for item in material_request.items:
-                if item.item_group == item_group:
-                    sorted_pick_list.append('locations', {   
-                        'item_code': frappe.db.get_value('Material Request Item', {'name': item.name}, 'item_code'),
-                        'stock_qty': frappe.db.get_value('Material Request Item', {'name': item.name}, 'qty  '),
-                        'material_request': material_request.name,
-                        'material_request_item': item.name,
-                        "conversion_factor": item.conversion_factor,
-                        "uom": item.uom
-                    })
+                try:
+                    sorted_pick_list.insert()
+                    frappe.db.commit()
 
-            def capture_msgprint(message, *args, **kwargs):
-                if "is not available in any of the warehouses" in message:
-                    out['message'] += f"Insufficient stock for item: {message}<br>"
-                elif "is picked in another Pick List" in message:
-                    out['message'] += f"Item already picked: {message}<br>"
+                except Exception as e:
+                    frappe.db.rollback()
+                    frappe.log_error(frappe.get_traceback(), "Error in create_pick_lists()")
+                    continue
+                
+                url = f'/app/material-request/{sorted_pick_list.material_request}'
+                link = f'<a href="{url}" target="_blank">{sorted_pick_list.material_request}</a>'
+                sorted_pick_list.add_comment('Info', f'Item Group: {sorted_pick_list.custom_item_group} - Split from Material Request: {link}')
 
-            frappe.msgprint = capture_msgprint
+            except Exception as e:
+                frappe.db.savepoint('sp5')
 
-            sorted_pick_list.insert()
-            url = f'/app/material-request/{sorted_pick_list.material_request}'
-            link = f'<a href="{url}" target="_blank">{sorted_pick_list.material_request}</a>'
-            sorted_pick_list.add_comment('Info', f'Item Group: {sorted_pick_list.custom_item_group} - Split from Material Request: {link}')
-            out['message'] += f'material request {material_request.set_warehouse} {cur_ct}/{ct} | inserted sorted pick list {item_group} {cur_pct}/{pct}<br>'
-            cur_pct += 1
+                try:
+                    sorted_pick_list.delete()
+                    frappe.db.commit()
+                    continue
 
-        except Exception as e:
-            sorted_pick_list.delete()
-            out['message'] += f'material request {material_request.set_warehouse} {cur_ct}/{ct} | error when inserting sorted pick list {item_group} {cur_pct}/{pct}: {e}<br>'
-            out['message'] += f'material request {material_request.set_warehouse} {cur_ct}/{ct} | deleted sorted pick list {item_group} {cur_pct}/{pct}<br>'
-            cur_pct += 1
-            continue
+                except Exception as e:
+                    frappe.db.rollback()
+                    frappe.log_error(frappe.get_traceback(), "Error in set_pick_lists()")
+                    continue
--- jollys_receiving/stock_audit/doctype/auto_reorder_settings/auto_reorder_settings.js
+++ jollys_receiving/stock_audit/doctype/auto_reorder_settings/auto_reorder_settings.js
@@ -2,24 +2,40 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Auto Reorder Settings", {
+	onload: function(frm) {
+        // Hides Add Row button from schedule child table
+        frm.get_field('schedule').grid.cannot_add_rows = true;
+        // Hides Delete button from schedule child table
+        frm.set_df_property('schedule', 'cannot_delete_rows', 1);
+        // Hides Add Row button from locations child table
+        frm.get_field('locations').grid.cannot_add_rows = true;
+        // Hides Delete button from locations child table
+        frm.set_df_property('locations', 'cannot_delete_rows', 1);
+    },
+
 	refresh: function(frm) {
+        $('div[class="col grid-static-col d-flex justify-content-center"]').remove();
+        $('div[class="col"]').remove();
+        $('.btn-open-row').hide();
+        $('.row-check').hide();
+
         frm.add_custom_button('Generate Auto Reorders', function() {
             frappe.confirm(
                 'Are you sure you wish to enqueue the generation of material requests based on current item reorder levels and quantities in the selected locations?',
                 () => {
                     frappe.call({
-                        method: 'jollys_receiving.public.set_picklists.schedule_set_picklists',
+                        method: 'jollys_receiving.public.set_picklists.schedule_generate_auto_reorder_material_requests',
                         freeze: true,
                         freeze_message: 'Enqueuing...',
                         callback: (r) => {
-                            if(r.message['status']) {
+                            if(!r.exc) {
                                 frappe.show_alert({
                                     message: 'Enqueued the generation of material requests based on current item reorder levels and quantities in the selected locations.',
                                     indicator:'green'
                                 }, 10);
                             } else {
                                 frappe.show_alert({
-                                    message: `Something went wrong: ${r.message['message']}`,
+                                    message: `Something went wrong: ${r.exc}`,
                                     indicator:'red'
                                 }, 10);
                             }
